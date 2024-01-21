8034536ee67cf9d2e9e833c9eacc8e4f41de943b
SietseDEV <110413038+SietseDEV@users.noreply.github.com>
2024-01-21 16:06:42 +0100

    Update news.js

--- api/news.js
+++ api/news.js
@@ -3,12 +3,10 @@ const fetch = require('node-fetch');
 export default async function handler(req, res) {
   try {
     const apiKey = process.env.NEWS_API_KEY;
-    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey}`; 
+    const apiUrl = `https://newsapi.org/v2/top-headlines?country=us`; 
 
     const response = await fetch(`${apiUrl}?apiKey=${apiKey}`);
     const data = await response.json();
-
-    console.log(`${apiUrl}?apiKey=${apiKey}`);
 
     res.status(200).json(data);
   } catch (error) {
