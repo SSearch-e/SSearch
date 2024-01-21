474c06cdd2e15c3307635845da0a7bb9893fc3eb
SietseDEV <110413038+SietseDEV@users.noreply.github.com>
2024-01-21 16:01:32 +0100

    Update news.js

--- api/news.js
+++ api/news.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
     const response = await fetch(`${apiUrl}?apiKey=${apiKey}`);
     const data = await response.json();
 
-    res.status(200).json(data);
+    res.status(200).json([[apiKey]);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Internal Server Error' });
